e13d5fef313fdada3d641a5c5895542a4ed6d314
Suhail <suhailece@gmail.com>
2024-04-20 11:34:55 -0700

    update __init__.py

---
+++ houdiniutils/__init__.py
@@ -0,0 +1,9 @@
+from .usdTools import *
+from .projTools import *
+from .importhou import *
+from .hda import *
+from .fSpy import *
+from .houdiniNet import *
+
+__all__ = ['usdTools', 'projTools', 'importhou', 'hda', 'fSpy', 'houdiniNet']
+
