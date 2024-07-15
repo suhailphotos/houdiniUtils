757435dc7fcf3be63d90fe857f8640455e47bc59
Suhail <suhailece@gmail.com>
2024-07-14 18:24:52 -0700

    update scripts

--- houdiniutils/__init__.py
+++ houdiniutils/__init__.py
@@ -4,6 +4,8 @@ from .importhou import *
 from .hda import *
 from .fSpy import *
 from .houdiniNet import *
+from .assetGallery import *
+from .sceneTools import *
 
-__all__ = ['usdTools', 'projTools', 'importhou', 'hda', 'fSpy', 'houdiniNet']
+__all__ = ['usdTools', 'projTools', 'importhou', 'hda', 'fSpy', 'houdiniNet', 'assetGallery', 'sceneTools']
 
